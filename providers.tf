889a612f799a94e66005846a6cb01dd02ef30880
gkranasinghe <gkranasinghe@gmail.com>
2022-09-10 16:02:56 +0530

    fake

--- providers.tf
+++ providers.tf
@@ -1,13 +1,3 @@
 provider "aws" {
-  region                   = var.aws_region
-  # profile                  = "A4L"
-  # shared_credentials_files = ["/home/gk/.aws/credentials_A4L"]
-
-  # assume_role {
-  #   role_arn = "arn:aws:iam::${var.aws_account_id}:role/terraform_admin_access"
-  # }
-
-  # assume_role {
-  #   role_arn = "arn:aws:iam::${lookup(var.aws_account_id, var.tag_environment)}:role/MYASSUMEROLE"
-  # }
+  region = var.aws_region
 }
