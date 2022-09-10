e1211f1734348ee31bdcb5c110603dc86f5a7085
gkranasinghe <gkranasinghe@gmail.com>
2022-09-10 15:57:37 +0530

    remote backend

--- providers.tf
+++ providers.tf
@@ -1,11 +1,11 @@
 provider "aws" {
   region                   = var.aws_region
-  profile                  = "A4L"
-  shared_credentials_files = ["/home/gk/.aws/credentials_A4L"]
+  # profile                  = "A4L"
+  # shared_credentials_files = ["/home/gk/.aws/credentials_A4L"]
 
-  assume_role {
-    role_arn = "arn:aws:iam::${var.aws_account_id}:role/terraform_admin_access"
-  }
+  # assume_role {
+  #   role_arn = "arn:aws:iam::${var.aws_account_id}:role/terraform_admin_access"
+  # }
 
   # assume_role {
   #   role_arn = "arn:aws:iam::${lookup(var.aws_account_id, var.tag_environment)}:role/MYASSUMEROLE"
---
+++ versions.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.26.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = ">= 1.1.0"
+
+  # cloud {
+  #   organization = "gkranasinghe"
+
+  #   workspaces {
+  #     name = "gh-actions-demo"
+  #   }
+  # }
+
+  backend "remote" {
+    organization = "gkranasinghe"
+
+    workspaces {
+      prefix = "gkranasinghe-"
+    }
+  }
+
+}
